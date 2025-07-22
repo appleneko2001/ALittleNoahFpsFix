bbe264b0382395eae7e4b76feb552f0087da64cc
appleneko2001 <appleneko2001@outlook.com>
2025-07-22 20:53:56 +0900

    Update local code changes and update README

--- Config.cs
+++ Config.cs
@@ -16,20 +16,9 @@ public partial class ALittleNoahFix
         public static ConfigEntry<int>    _iForcedLodQuality;           // Default is 0, goes up to LOD #3 without cutting insane amounts of level geometry.
         public static ConfigEntry<int>    _iForcedTextureQuality;       // Default is 0, goes up to 1/14th resolution.
         public static ConfigEntry<int>    _anisotropicFiltering;        // 0: Off, 2: 2xAF, 4: 4xAF, 8: 8xAF, 16: 16xAF.
-        // Little Noah specific options.
-        public static ConfigEntry<bool> _bDepthOfField;                 // Quick Toggle for Depth of Field.
-        public static ConfigEntry<bool> _bBloom;                        // Quick Toggle for Bloom.
-        public static ConfigEntry<bool> _bChromaticAberration;          // Quick Toggle for Chromatic Aberration.
-        public static ConfigEntry<bool> _bLensDistortion;               // Quick Toggle for Lens Distortion.
-        public static ConfigEntry<bool> _bVignette;                     // Quick Toggle for Vignette.
 
         // Framelimiter Config
         public static ConfigEntry<bool> _bvSync; // Self Explanatory. Prevents the game's framerate from going over the screen refresh rate, as that can cause screen tearing or increased energy consumption.
-        
-        // Resolution Config
-        public static          ConfigEntry<bool> _bForceCustomResolution;
-        public static          ConfigEntry<int>  _iHorizontalResolution;
-        public static          ConfigEntry<int>  _iVerticalResolution;
         
         // Misc Config
         //public static ConfigEntry<bool> _bSkipSplashScreenSequence; // True: Skips Opening Splash Screen Logos and Movie, False: Default Behavior.
@@ -42,20 +31,10 @@ public partial class ALittleNoahFix
             _iForcedLodQuality = Config.Bind("Graphics", "LOD Quality", 0, new ConfigDescription("0: No Forced LODs (Default), 1: Forces LOD # 1, 2: Forces LOD # 2, 3: Forces LOD # 3. Higher the value, the less mesh detail.", new AcceptableValueRange<int>(0, 3)));
             _iForcedTextureQuality = Config.Bind("Graphics", "Texture Quality", 0, new ConfigDescription("0: Full Resolution (Default), 1: Half-Res, 2: Quarter Res. Goes up to 1/14th res (14).", new AcceptableValueRange<int>(0, 14)));
             _anisotropicFiltering = Config.Bind("Graphics", "Anisotropic Filtering", 0, new ConfigDescription("0: Off, 2: 2xAF, 4: 4xAF, 8: 8xAF, 16: 16xAF", new AcceptableValueRange<int>(0, 16)));
-            _bDepthOfField        = Config.Bind("Graphics", "Depth of Field", true, new ConfigDescription("On: Enables Depth of Field (Default), Off: Disables Depth of Field."));
-            _bBloom               = Config.Bind("Graphics", "Bloom", true, new ConfigDescription("On: Enables Bloom (Default), Off: Disables Bloom."));
-            _bChromaticAberration = Config.Bind("Graphics", "Chromatic Aberration", true, new ConfigDescription("On: Enables Chromatic Aberration (Default), Off: Disables Chromatic Aberration."));
-            _bLensDistortion      = Config.Bind("Graphics", "Lens Distortion", true, new ConfigDescription("On: Enables Lens Distortion (Default), Off: Disables Lens Distortion."));
-            _bVignette            = Config.Bind("Graphics", "Vignette", true, new ConfigDescription("On: Enables Vignette (Default), Off: Disables Vignette."));
-            
+
             // Framelimiter Config
             _bvSync = Config.Bind("Framerate", "VSync", true, "Self Explanatory. Prevents the game's framerate from going over the screen refresh rate, as that can cause screen tearing or increased energy consumption.");
-            
-            // Resolution Config
-            _bForceCustomResolution = Config.Bind("Resolution", "Force Custom Resolution", false, "Self Explanatory. A temporary toggle for custom resolutions until I can figure out how to go about removing the resolution count restrictions.");
-            _iHorizontalResolution  = Config.Bind("Resolution", "Horizontal Resolution",   Screen.currentResolution.m_Width);
-            _iVerticalResolution    = Config.Bind("Resolution", "Vertical Resolution",     Screen.currentResolution.m_Height);
-            
+
             // Misc Config
             //_bSkipSplashScreenSequence = Config.Bind("Misc", "Skip Splash Screens and Opening Video", false, "True: Skips Splash Screen and Opening Videos for faster startup times, False: Default Functionality.");
         }
--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# ALittleNoahFix
-PC Port Improvement Mod for Little Noah: Scion of Paradise.
+# ALittleNoahFpsFix
+A fork of [KingKrouch's ALittleNoahFix](https://github.com/KingKrouch/ALittleNoahFix), but everything have been removed except framerate unlocker feature remains and improved a bit.
+
+# Compile
+
+Require .NET 6.0 or newer
+
+```shell
+# clone the repository and open the directory
+git clone https://github.com/appleneko2001/ALittleNoahFpsFix
+cd ALittleNoahFpsFix
+
+# restore dependencies
+dotnet restore
+
+# build
+dotnet build
+```
+
+# Usage
+
+<!-- Install instructions backup:
+Download the ALittleNoahFix.zip file from the GitHub releases section.
+Extract the contents into the game's install directory.
+If running on Linux or the Steam Deck, add
+`WINEDLLOVERRIDES="winhttp=n,b" %command%`
+to the game's launch parameters in the game's Steam properties.
+-->
+
+> [!IMPORTANT]
+> Please compile the solution first. 
+> 
+> After compile should appear a folder `bin` under repository folder. Open `Debug`, `net6.0` and take out `ALittleNoahFix.dll` and `ALittleNoahFix.deps.json` (and `ALittleNoahFix.pdb` if you need to symbol debugging) for use later.
+
+1. Follow "Install Instructions" first three steps: 
+https://steamcommunity.com/app/1883260/discussions/0/4358998644635061533
+
+2. Replace files in games folder `BepInEx/plugins` with files that you compiled before.
+3. Launch the game and disable V-Sync settings in game. Its broken once you have applied this patch.
+4. Optional. Change configuration file in `BepInEx/config/ALittleNoahFix.cfg` if necessary. Only VSync settings is usable since I have removed all features of upstream patches.
+
+Once you complete those steps your game should have been framerate unlocked. You can also disable VSync in configuration file to let the game go higher framerate.
