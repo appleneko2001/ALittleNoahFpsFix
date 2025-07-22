d1dd7f4ee889c3f42e92a6e9e042fb850fbcfa6e
appleneko2001 <appleneko2001@outlook.com>
2025-07-22 20:53:56 +0900

    Update local code changes and update README
    (cherry picked from commit bbe264b0382395eae7e4b76feb552f0087da64cc)

--- Plugin.cs
+++ Plugin.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Linq;
-using BepInEx;
-using BepInEx.Configuration;
+﻿using BepInEx;
 using BepInEx.Logging;
 using BepInEx.Unity.IL2CPP;
-using Game;
 using Game.Core;
-using Game.Rendering;
 using HarmonyLib;
 using UnityEngine;
-using UnityEngine.Rendering;
 using UnityEngine.UI;
-using UnityEngine.Rendering.Universal;
-using UnityEngine.Video;
 
 namespace ALittleNoahFix;
 
@@ -34,40 +26,8 @@ public partial class ALittleNoahFix : BasePlugin
         
         // Finally, load our patches.
         Harmony.CreateAndPatchAll(typeof(MousePatches));
-        Harmony.CreateAndPatchAll(typeof(ResolutionPatches));
         Harmony.CreateAndPatchAll(typeof(UIPatches));
         Harmony.CreateAndPatchAll(typeof(GraphicsPatches));
-        //Harmony.CreateAndPatchAll(typeof(SteamPatches));
-        //Harmony.CreateAndPatchAll(typeof(CameraPatches));
-    }
-
-    [HarmonyPatch]
-    public class SteamPatches
-    {
-        
-    }
-
-    [HarmonyPatch]
-    public class ResolutionPatches
-    {
-        
-        [HarmonyPatch(typeof(Game.TitleScene), nameof(Game.TitleScene.Start)), HarmonyPostfix]
-        //[HarmonyPatch(typeof(Game.SystemDataStatus), nameof(Game.SystemDataStatus.UpdateResolution)), HarmonyPrefix]
-        public static void ForceCustomResolution()
-        {
-            if (_bForceCustomResolution.Value) {
-                Screen.SetResolution(_iHorizontalResolution.Value, _iVerticalResolution.Value, Screen.fullScreenMode);
-                Debug.Log("Resolution Value Changed to: " + Screen.currentResolution.m_Width + "x" + Screen.currentResolution.m_Height + ".");
-            }
-            return;
-        }
-
-        [HarmonyPatch(typeof(OptionGamePlayMenuElementResolution), nameof(OptionGamePlayMenuElementResolution.Setup)), HarmonyPostfix]
-        public static void SetupResolutions(OptionGamePlayMenuElementResolution __instance)
-        {
-            // TODO: Override the in-game resolution options in favor of our own.
-            return;
-        }
     }
 
     [HarmonyPatch]
@@ -87,79 +47,34 @@ public partial class ALittleNoahFix : BasePlugin
     [HarmonyPatch]
     public class GraphicsPatches
     {
-        [HarmonyPatch(typeof(Volume), nameof(Volume.Update))]
-        [HarmonyPostfix]
-        public static void PatchPostProcessing(ref Volume __instance)
-        {
-            if (__instance == null) return;
-            var volumeProfile = __instance.profile;
-            if (volumeProfile == null) return;
-            volumeProfile.TryGet(out DepthOfField dof);
-            if (dof) {
-                dof.active = _bDepthOfField.Value switch {
-                    true  => true,
-                    false => false
-                };
-            }
-            
-            volumeProfile.TryGet(out Bloom bloom);
-            if (bloom) {
-                bloom.active = _bBloom.Value switch {
-                    true  => true,
-                    false => false
-                };
-            }
-            
-            volumeProfile.TryGet(out ChromaticAberration ca);
-            if (ca) {
-                ca.active = _bChromaticAberration.Value switch {
-                    true  => ca.active,
-                    false => false
-                };
-            }
-            
-            volumeProfile.TryGet(out LensDistortion ld);
-            if (ld) {
-                ld.active = _bLensDistortion.Value switch {
-                    true  => ld.active,
-                    false => false
-                };
-            }
-            
-            volumeProfile.TryGet(out Vignette vg);
-            if (vg) {
-                vg.active = _bVignette.Value switch {
-                    true  => vg.active,
-                    false => false
-                };
-            }
-        }
+        private static int m_PreviousRefreshRate;
+        private static bool m_PreviousVSyncEnabled;
         
         [HarmonyPatch(typeof(Engine), nameof(Engine.DelayFrame)), HarmonyPrefix]
         public static bool PatchFramerateLimiter()
         {
+            var refreshRate = Screen.currentResolution.m_RefreshRate;
+            var vsync = _bvSync.Value;
+            
+            // Apply changes only when target display refresh rate is changed.
+            // For example, new screen settings was 1920x1080@144hz, but old one was 1280x720@60hz
+            // then the patch will be re-applied once
+            if (m_PreviousRefreshRate == refreshRate && m_PreviousVSyncEnabled == vsync)
+                return false;
+            
+            var fixedDt = 1.0f / refreshRate;
             // Let us adjust VSync.
-            QualitySettings.vSyncCount = _bvSync.Value ? 1 : 0;
+            QualitySettings.vSyncCount = vsync ? 1 : 0;
             // Unlock the Framerate.
-            Application.targetFrameRate = -1;
+            Application.targetFrameRate = vsync ? refreshRate : -1;
             // FixedDeltaTime is seemingly being used by most things instead of DeltaTime, which is why I assume the shitty framelimiter has been added. Anyways, after this, smooth as butter.
-            Time.fixedDeltaTime = 1.0f / Screen.currentResolution.m_RefreshRate;
+            Time.fixedDeltaTime = fixedDt;
+            
+            Debug.Log($"FPS Patch status:\r\nRefreshRate: {refreshRate}\r\nV-Sync: {vsync}\r\nV-Sync count: {QualitySettings.vSyncCount}");
+            m_PreviousRefreshRate = refreshRate;
+            m_PreviousVSyncEnabled = vsync;
             return false;
         }
-        // This should in theory patch the camera to use Vert+ Scaling.
-        // TODO: Fix this.
-        [HarmonyPatch(typeof(Game.BattleCamera), nameof(Game.BattleCamera.CalcCameraFov), MethodType.Setter), HarmonyPostfix]
-        public static void PatchCameraFOV(Game.BattleCamera __instance)
-        {
-            var oldFOV = __instance.mCamera.fieldOfView;
-            __instance.mCamera.usePhysicalProperties = true;
-            __instance.mCamera.sensorSize = new Vector2(16, 9);
-            __instance.mCamera.gateFit = Camera.GateFitMode.Overscan;
-            __instance.mCamera.fieldOfView = oldFOV;
-        }
-        // Game.MiniMap -> Parent (GameObject name: Info), this is where the minimap scale can be adjusted for lower resolutions.
-        // Game.Gameplay.EventDialog can have it's positioning set lower with resolutions narrower than 16:9 to prevent clipping problems.
-        // or it can have it's anchor set to the lower center of the screen, so it's consistent and less math is needed, maybe...
     }
 
     [HarmonyPatch]
