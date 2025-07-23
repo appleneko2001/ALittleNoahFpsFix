80b62fcfcd9dc87bafe0539b1258c6edd2edbae7
appleneko2001 <appleneko2001@outlook.com>
2025-07-23 23:38:36 +0900

    Add throttle framerate on window inactivate state patch

--- Plugin.cs
+++ Plugin.cs
@@ -42,36 +42,81 @@ public partial class ALittleNoahFix : BasePlugin
         }
     }
 
+    // TODO: framerate is stable but game state update is not
+    // TODO: alternative framerate lock in background mode (not front focused window state)
     [HarmonyPatch]
     public class GraphicsPatches
     {
-        private static int m_PreviousRefreshRate;
         private static bool m_PreviousVSyncEnabled;
-        
+        private static int? m_FramerateThrottler;
+
         [HarmonyPatch(typeof(Engine), nameof(Engine.DelayFrame)), HarmonyPrefix]
         public static bool PatchFramerateLimiter()
         {
             var refreshRate = Screen.currentResolution.m_RefreshRate;
             var vsync = _bvSync.Value;
-            
+
+            GetInactiveWindowThrottle();
+            var throttle = m_FramerateThrottler ?? -1;
+
+            if (throttle > 0)
+            {
+                if (Application.targetFrameRate == throttle)
+                    return false;
+
+                Application.targetFrameRate = throttle;
+                Debug.Log($"Throttler applied");
+                return false;
+            }
+
+            UpdateFramerateLimiterStatePrivate(refreshRate, vsync);
+            m_PreviousVSyncEnabled = vsync;
+            return false;
+        }
+
+        private static void UpdateFramerateLimiterStatePrivate(int refreshRate, bool vsync)
+        {
             // Apply changes only when target display refresh rate is changed.
             // For example, new screen settings was 1920x1080@144hz, but old one was 1280x720@60hz
             // then the patch will be re-applied once
-            if (m_PreviousRefreshRate == refreshRate && m_PreviousVSyncEnabled == vsync)
-                return false;
-            
+            if (Application.targetFrameRate == refreshRate && m_PreviousVSyncEnabled == vsync)
+                return;
+
             var fixedDt = 1.0f / refreshRate;
             // Let us adjust VSync.
             QualitySettings.vSyncCount = vsync ? 1 : 0;
             // Unlock the Framerate.
             Application.targetFrameRate = vsync ? refreshRate : -1;
-            // FixedDeltaTime is seemingly being used by most things instead of DeltaTime, which is why I assume the shitty framelimiter has been added. Anyways, after this, smooth as butter.
+            // FixedDeltaTime is seemingly being used by most things instead of DeltaTime,
+            // which is why I assume the shitty framelimiter has been added. Anyways, after this, smooth as butter.
+
+            // BUT ALSO BUGGY IF THE REFRESH RATE IS TOO HIGH
+            // WHY NOT DELTA TIME BUT FIXED DELTA TIME?!
+            // DAMN GAME DEVS
             Time.fixedDeltaTime = fixedDt;
-            
-            Debug.Log($"FPS Patch status:\r\nRefreshRate: {refreshRate}\r\nV-Sync: {vsync}\r\nV-Sync count: {QualitySettings.vSyncCount}");
-            m_PreviousRefreshRate = refreshRate;
-            m_PreviousVSyncEnabled = vsync;
-            return false;
+
+            Debug.Log($"FPS Patch status:\r\nUpdate frequency: {fixedDt}ms\r\nRefreshRate: {refreshRate}\r\nV-Sync: {vsync}\r\nV-Sync count: {QualitySettings.vSyncCount}");
+        }
+
+        private static void GetInactiveWindowThrottle()
+        {
+            if (Application.isFocused)
+            {
+                if (m_FramerateThrottler == null)
+                    return;
+
+                m_FramerateThrottler = null;
+            }
+            else
+            {
+                const int target = 30;
+
+                if(m_FramerateThrottler == target)
+                    return;
+
+                m_FramerateThrottler = target;
+            }
+            Debug.Log($"Throttler status: {m_FramerateThrottler?.ToString() ?? "disabled"}");
         }
     }
 
